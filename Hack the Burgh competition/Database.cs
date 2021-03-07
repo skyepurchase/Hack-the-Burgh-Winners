fe3fe53f3ea345a20285047033c4778fbf9b67e1
Skye <aidantpurchase@gmail.com>
2021-03-07 00:38:53 +0000

    Added getting consecutive days for opening and closing values

--- Hack the Burgh competition/Database.cs
+++ Hack the Burgh competition/Database.cs
@@ -6,34 +6,41 @@ namespace Hack_the_Burgh_competition
 {
     class Database
     {
-        private Dictionary<string, Dictionary<string, string>> stocks = new Dictionary<string, Dictionary<string, string>>();
+        private Dictionary<string, Dictionary<DateTime, string>> closeStocks = new Dictionary<string, Dictionary<DateTime, string>>();
+        private Dictionary<string, Dictionary<DateTime, string>> openStocks = new Dictionary<string, Dictionary<DateTime, string>>();
         private string[] stockTypes = { "Tech", "Oil", "Entertainment", "Travel", "Gold" };  // Hard code 5 types
 
         public Database()
         {
             foreach (string type in stockTypes)
             {
-                string[] csvLines = File.ReadAllLines($"stockdata{type}.csv");
+                string[] csvLinesClose = File.ReadAllLines($"closestockdata{type}.csv");
+                string[] csvLinesOpen = File.ReadAllLines($"openstockdata{type}.csv");
 
-                for (int i = 0; i < csvLines.Length; i++)
+                for (int i = 0; i < csvLinesClose.Length; i++)
                 {
-                    string[] rowData = csvLines[i].Split(',');
-                    stocks[type].Add(rowData[0], rowData[1]);
+                    string[] rowData = csvLinesClose[i].Split(',');
+                    string[] stringDate = rowData[0].Split('-');
+                    int year = int.Parse(stringDate[0]);
+                    int day = int.Parse(stringDate[1]);
+                    int month = int.Parse(stringDate[2]);
+                    DateTime date = new DateTime(year, month, day);
+                    closeStocks[type].Add(date, rowData[1]);
+                }
+                for (int i = 0; i < csvLinesOpen.Length; i++)
+                {
+                    string[] rowData = csvLinesOpen[i].Split(',');
+                    string[] stringDate = rowData[0].Split('-');
+                    int year = int.Parse(stringDate[0]);
+                    int day = int.Parse(stringDate[1]);
+                    int month = int.Parse(stringDate[2]);
+                    DateTime date = new DateTime(year, month, day);
+                    openStocks[type].Add(date, rowData[1]);
                 }
             }
         }
 
-        /*public int buyStock(string type, string date, int number)
-        {
-            return this.stocks[type].buyStock(date, number);
-        }
-
-        public int sellStock(string type, string date, int number)
-        {
-            return this.stocks[type].sellStock(date, number);
-        }*/
-
-        public Dictionary<string, string> GetOpenConsecutiveDays(string type, string startDate, int days)
+        public Dictionary<string, string> GetConsecutiveDays(string type, string startDate, int days)
         {
             string[] stringDate = startDate.Split('-');
             int year = int.Parse(stringDate[0]);
@@ -42,6 +49,61 @@ namespace Hack_the_Burgh_competition
             DateTime date = new DateTime(year, month, day);
 
             Dictionary<string, string> result = new Dictionary<string, string>();
+
+            for (int i = 0; i < days; i++)
+            {
+                date = date.AddDays(1);
+                string key = date.Year.ToString() + '-' + date.Day.ToString() + '-' + date.Month.ToString();
+                if (this.openStocks[type].ContainsKey(date) && this.closeStocks[type].ContainsKey(date))
+                {
+                    result[key] = this.openStocks[type][date] + ',' + this.closeStocks[type][date];
+                } 
+            }
+
+            return result;
+        }
+
+        public Dictionary<string, string> GetConsecutiveMonths(string type, string startDate, int months)
+        {
+            string[] stringDate = startDate.Split('-');
+            int year = int.Parse(stringDate[0]);
+            int month = int.Parse(stringDate[2]);
+            DateTime date = new DateTime(year, month, 1);
+
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            for (int i = 0; i < months; i++)
+            {
+                date = date.AddMonths(1);
+                DateTime openDate = date.AddDays(-1);
+                string key = date.Year.ToString() + '-' + date.Month.ToString();
+                if (this.openStocks[type].ContainsKey(openDate) && this.closeStocks[type].ContainsKey(date))
+                {
+                    result[key] = this.openStocks[type][openDate] + ',' + this.closeStocks[type][date];
+                }
+            }
+
+            return result;
+        }
+
+        public Dictionary<string, string> GetConsecutiveYears(string type, string startDate, int days)
+        {
+            string[] stringDate = startDate.Split('-');
+            int year = int.Parse(stringDate[0]);
+            DateTime date = new DateTime(year, 1, 1);
+
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            for (int i = 0; i < days; i++)
+            {
+                date = date.AddYears(1);
+                DateTime openDate = date.AddDays(-1);
+                string key = date.Year.ToString();
+                if (this.openStocks[type].ContainsKey(openDate) && this.closeStocks[type].ContainsKey(date))
+                {
+                    result[key] = this.openStocks[type][openDate] + ',' + this.closeStocks[type][date];
+                }
+            }
 
             return result;
         }
